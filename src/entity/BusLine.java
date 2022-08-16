ac23258e7c1024a3ff9b941a9f77941c635b17b6
hoanglan <hoanglan.nguyen0208@gmail.com>
2022-08-16 15:35:11 +0900

    Initial commit

---
+++ src/entity/BusLine.java
@@ -0,0 +1,71 @@
+package entity;
+import java.io.Serializable;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import java.io.Serializable;
+
+public class BusLine implements AutoIdIncreasable, Serializable, NewDataCreatable {
+
+    private static int AUTO_ID = 100;
+
+    private int id;
+    private float distance;
+    private int stopStationNumber;
+
+    public BusLine() {
+        this.increaseId();
+    }
+
+    public BusLine(float distance, int stopStationNumber) {
+        this.distance = distance;
+        this.stopStationNumber = stopStationNumber;
+        this.increaseId();
+    }
+
+    @Override
+    public final void increaseId() {
+        this.id = AUTO_ID++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public float getDistance() {
+        return distance;
+    }
+
+    public void setDistance(float distance) {
+        this.distance = distance;
+    }
+
+    public int getStopStationNumber() {
+        return stopStationNumber;
+    }
+
+    public void setStopStationNumber(int stopStationNumber) {
+        this.stopStationNumber = stopStationNumber;
+    }
+
+    @Override
+    public void inputNewData(){
+        System.out.println("Nhập khoảng cách của tuyến xe: ");
+        float distance = 0;
+        do {
+            try {
+                distance = new Scanner(System.in).nextFloat();
+            } catch (InputMismatchException ex) {
+                System.out.println("khoảng cách phải là số nguyên, yêu cầu nhập lại: ");
+                continue;
+            }
+            if (distance > 0){
+                this.setDistance(distance);
+                break;
+            }
+            System.out.println("Khoảng cách của tuyến mới không được là số âm, yêu cầu nhập lại");
+        } while (true);
+
+        System.out.println("Nhập số điểm dừng của tuyến xe: ");
+    }
+}
